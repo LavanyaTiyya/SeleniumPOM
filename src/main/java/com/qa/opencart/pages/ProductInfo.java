38daa00c23f30d6e9354b0106758a0426619c958
Lavanya <nimmalalavanya@gmail.com>
2022-07-18 23:15:07 +0530

    added product info

---
+++ src/main/java/com/qa/opencart/pages/ProductInfo.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.opencart.utils.ElementUtil;
+
+public class ProductInfo {
+
+	private WebDriver driver;
+	private ElementUtil eleUtil;
+}
